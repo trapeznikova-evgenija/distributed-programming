2616a8600b54ddb48720841bf61eb58834972571
trapeznikova-evgenija <trapeznikiva.evg@yandex.ru>
2019-05-29 14:15:13 +0300

    скрипты сборки

---
+++ build.cmd
@@ -0,0 +1,39 @@
+set version=%~1
+set currentPath=%CD%
+set buildFolderDir=version_%version%
+
+set backendFolderName=Backend
+set frontendFolderName=Frontend
+
+if %1 == "" goto write_version
+if exist "%currentPath%\%backendFolderName%" goto build_exists
+mkdir "%buildFolderDir%"
+call :create_run_script
+call :create_stop_script
+call :start_build
+
+:write_version
+echo write application version
+exit
+
+:build_exists
+echo %version% folder already exist
+exit
+
+:create_run_script
+  (
+    echo start /d %frontendFolderName% dotnet %frontendFolderName%.dll
+    echo start /d %backendFolderName% dotnet %backendFolderName%.dll 
+  ) > "%buildFolderDir%/run.cmd"
+exit /b 0
+
+:create_stop_script
+  (
+    echo taskkill /im dotnet.exe
+  ) > "%buildFolderDir%/stop.cmd"
+exit /b 0
+
+:start_build
+dotnet build "src/%backendFolderName%" -o "../../%buildFolderDir%/%backendFolderName%"
+dotnet build "src/%frontendFolderName%" -o "../../%buildFolderDir%/%frontendFolderName%"
+exit /b 0
---
+++ version_3.2.8/run.cmd
@@ -0,0 +1,2 @@
+start /d Frontend dotnet Frontend.dll
+start /d Backend dotnet Backend.dll 
